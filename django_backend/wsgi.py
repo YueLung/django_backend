f5a4f2828bfaceb109780b23a9cff140ef71c947
YueLung <ss90607@gmail.com>
2022-09-18 11:58:49 +0800

    modify

--- django_backend/wsgi.py
+++ django_backend/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_backend.settings')
 
-# application = get_wsgi_application()
-application = Cling(get_wsgi_application())  # heroku setting
+application = get_wsgi_application()
