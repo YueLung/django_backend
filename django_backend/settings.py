1768a33a8f2ba2baf7d7c84199fd3ad8f4c30bdb
YueLung <ss90607@gmail.com>
2022-09-18 12:37:07 +0800

    modify

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -35,12 +35,13 @@ LINE_CHANNEL_ACCESS_TOKEN = env('LINE_CHANNEL_ACCESS_TOKEN')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['yuelung-django.herokuapp.com/']
+ALLOWED_HOSTS = []
 
-# if DEBUG == False:
-#     ALLOWED_HOSTS += [
-#         'yuelung-django.herokuapp.com/'
-#     ]
+if DEBUG == False:
+    ALLOWED_HOSTS += [
+        'lung.pythonanywhere.com',
+        'yuelung-django.herokuapp.com'
+    ]
 
 
 # Application definition
